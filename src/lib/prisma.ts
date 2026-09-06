26102cecf6f79e148f3cf78cb2723c022c851fde
tkkteam <lovebytumjaa@gmail.com>
2026-09-06 16:27:40 +0700

    fix data4

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import ws from 'ws'
+
+neonConfig.webSocketConstructor = ws
 
 const prismaClientSingleton = () => {
-  const connectionString = process.env.DATABASE_URL
+  const connectionString = process.env.DATABASE_URL!
   const pool = new Pool({ connectionString })
-  const adapter = new PrismaPg(pool)
+  const adapter = new PrismaNeon(pool)
   return new PrismaClient({ adapter })
 }
 
